6fe1b16d8ce570e1014f92db7d075aa4f17246ce
Nazarii Spikhalskyi <nazarii.spikhalskyi@ria.com>
2022-10-30 16:29:37 +0200

    add_Lecture_3_Home_task_1_and_2

---
+++ lecture-3/task-1.js
@@ -0,0 +1,24 @@
+function isInputValid(input) {
+    const strInput = String(input).trim();
+    const inputRegex = /^(\d{2})$/;
+    
+    if (strInput.match(inputRegex) === null) {
+        return false;
+    }
+    return true;
+};
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question('Write your number between 10 and 99: ', number => {
+    if (isInputValid(number) === false) {
+        console.log("Wrong input, should be a number between 10 and 99.");
+    } else {
+        console.log(String(number).trim().split("").join(" "));
+    }
+
+    readline.close();
+});
